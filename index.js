37fd5aab70226f3f29c9af08583a4f374367f743
Blissmal <malutibethuel@gmail.com>
2024-03-22 22:49:30 +0300

    updated features

--- index.js
+++ index.js
@@ -244,9 +244,13 @@ let temp
 
 function convert(){
     if(toFahrenheit.checked){
-
+        temp = Number(textBox.value)
+        temp = temp * 9 / 5 + 32
+        result.textContent = temp.toFixed(1) + "°F"
     }else if(toCelsius.checked){
-
+        temp = Number(textBox.value)
+        temp = (temp - 32) * (5 / 9)
+        result.textContent = temp.toFixed(1) + "°C"
     }else{
         result.textContent = `Select a unit`
     }
