9b05952de5be81ff1654ff1996b2ded69add7e08
Blissmal <malutibethuel@gmail.com>
2024-03-22 22:41:04 +0300

    updated

--- index.js
+++ index.js
@@ -238,10 +238,18 @@ console.log(add(4,6))
 const textBox = document.getElementById("textbox")
 const toFahrenheit = document.getElementById("toFahrenheit")
 const toCelsius = document.getElementById("toCelsius")
+const result = document.getElementById("result")
+let temp
 
 
 function convert(){
+    if(toFahrenheit.checked){
 
+    }else if(toCelsius.checked){
+
+    }else{
+        result.textContent = `Select a unit`
+    }
 }
 
 
