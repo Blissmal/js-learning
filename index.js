0178c6228545f03a5e9a3bd0e685cdd53dfd0142
Blissmal <malutibethuel@gmail.com>
2024-03-22 23:54:33 +0300

    update

--- index.js
+++ index.js
@@ -321,7 +321,10 @@ function rollDice(){
     for (let i = 0; i < numOfDice; i++){
         const value = Math.floor(Math.random() * 6) + 1
         values.push(value)
-        images.push(`<img src="dice_images">`)
+        images.push(`<img src="dice_images/${value}.svg" alt="Dice ${value}">`)
     }
+
+    diceResult.textContent = `dice: ${values.join(', ')}`
+    diceImages.innerHTML = images.join('')
 
 }
