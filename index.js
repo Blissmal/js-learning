6b04751d2bdcd84efce05fd6bf90a70b8f9e53cf
Blissmal <malutibethuel@gmail.com>
2024-03-23 23:30:03 +0300

    updates

--- index.js
+++ index.js
@@ -644,6 +644,7 @@ function start(){
    if(!isRuning){
     startTime = Date.now() - elapsedTime
     timer = setInterval(update, 10)
+    isRuning = true
    } 
 
 }
@@ -655,5 +656,17 @@ function reset(){
 
 }
 function update(){
+    const currentTime = Date.now()
+    elapsedTime = currentTime - startTime
+    let hours = Math.floor(elapsedTime / (1000 * 60 * 60))
+    let minutes = Math.floor(elapsedTime / (1000 * 60) % 60)
+    let seconds = Math.floor(elapsedTime / 1000 % 60)
+    let milliseconds = Math.floor(elapsedTime % 1000 / 10)
 
+    hours = String(hours).padStart(2, "0")
+    minutes = String(minutes).padStart(2, "0")
+    seconds = String(seconds).padStart(2, "0")
+    milliseconds = String(milliseconds).padStart(2, "0")
+
+    Display.textContent = `${hours}:${minutes}:${seconds}:${milliseconds}`
 }
