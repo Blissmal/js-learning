a50cb61285a8a3a1f0f005a53d2917a429100b23
Blissmal <malutibethuel@gmail.com>
2024-03-23 20:24:15 +0300

    updated

--- index.js
+++ index.js
@@ -463,7 +463,16 @@ console.log(`Total price (with tax): $${Total1.toFixed(2)}`)
 
 class MathUtil{
     static PI = 3.14159
+    static getDiameter(radius){
+        return radius * 2
+    }
+    
+    static getCircumference(radius){
+        return 2 * this.PI * radius
+    }
 }
 
 console.log(MathUtil.PI)
+console.log(MathUtil.getDiameter(10))
+console.log(MathUtil.getCircumference(7))
 
