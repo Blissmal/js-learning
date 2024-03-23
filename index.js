678f9059ab9d1c0307a2805c701213026d14f74a
Blissmal <malutibethuel@gmail.com>
2024-03-23 20:18:29 +0300

    updated

--- index.js
+++ index.js
@@ -433,4 +433,33 @@ console.log(car2.color)
 car1.drive()
 car2.drive()
 
+class Product{
+    constructor(name, price){
+        this.name = name
+        this.price = price
+    }
+
+    displayProduct(){
+        console.log(`Product: ${this.name}`)
+        console.log(`Price: ${this.price.toFixed(2)}`)
+    }
+    calculateTotal(salesTax){
+        return this.price + (this.price * salesTax)
+    }
+}
+
+const salesTax = 0.05
+
+const product1 = new Product("shirt", 19.99)
+const product2 = new Product("pants", 22.5)
+
+product1.displayProduct()
+const Total = product1.calculateTotal(salesTax)
+console.log(`Total price (with tax): $${Total.toFixed(2)}`)
+
+product2.displayProduct()
+const Total1 = product2.calculateTotal(salesTax)
+console.log(`Total price (with tax): $${Total1.toFixed(2)}`)
+
+
 
