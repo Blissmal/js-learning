072ac0a6249afce8f03eb6bebfabe2bcaf076519
Blissmal <malutibethuel@gmail.com>
2024-03-23 20:02:51 +0300

    updated

--- index.js
+++ index.js
@@ -415,5 +415,16 @@ function Car(make, model, year, color){
 }
 
 const car1 = new Car("Mercedes", "Benz", 2024, "Red")
+const car2 = new Car("Ford", "Mustang", 2023, "Blue")
+
+console.log(car1.make)
 console.log(car1.model)
+console.log(car1.year)
+console.log(car1.color)
+
+console.log(car2.make)
+console.log(car2.model)
+console.log(car2.year)
+console.log(car2.color)
+
 
