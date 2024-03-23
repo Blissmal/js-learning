ab6f4dbb551fd3566347ac329564f18f11c4526d
Blissmal <malutibethuel@gmail.com>
2024-03-23 20:07:40 +0300

    updated

--- index.js
+++ index.js
@@ -411,7 +411,10 @@ function Car(make, model, year, color){
     this.make = make,
     this.model = model,
     this.year = year,
-    this.color = color
+    this.color = color,
+    this.drive = () => {
+        console.log(`You drive the ${this.model}`)
+    } 
 }
 
 const car1 = new Car("Mercedes", "Benz", 2024, "Red")
@@ -426,5 +429,8 @@ console.log(car2.make)
 console.log(car2.model)
 console.log(car2.year)
 console.log(car2.color)
+
+car1.drive()
+car2.drive()
 
 
